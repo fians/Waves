b53c67d95e7983b9495ba21e441a713bb32641e6
Alfiana Sibuea <alfian.sibuea@gmail.com>
2015-04-19 20:23:19 +0700

    Add fallback for Waves.displayEffect()

--- src/js/waves.js
+++ src/js/waves.js
@@ -521,6 +521,14 @@
             Effect.hide(mouseup, elements[i]);
         }
     };
+    
+    /**
+     * Deprecated API fallback
+     */
+    Waves.displayEffect = function(options) {
+        console.error('Waves.displayEffect() has been deprecated and will be removed in future version. Please use Waves.init() to initialize Waves effect');
+        Waves.init(options);
+    };
 
     return Waves;
 });
