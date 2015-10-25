023e842893f6980c74fa79dddf0040632d5dfb83
Alfiana Sibuea <alfian.sibuea@gmail.com>
2015-10-25 23:08:02 +0700

    Fix grunt build less2stylus

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
                 call: function(grunt, options, async) {
                     var done = async();
                     var exec = require('child_process').exec;
-                    exec('cd node_modules/less2stylus && ./less2stylus ../../src/less/waves.less', function (error, stdout, stderr) {
+                    exec('cd node_modules/less2stylus && node ./less2stylus ../../src/less/waves.less', function (error, stdout, stderr) {
                         grunt.log.writeln('Executing less2styus...');
 
                         if (error) {
